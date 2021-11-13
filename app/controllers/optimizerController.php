c3cc2e83c10fc668c84fd980473778dd3c8a2d0e
Masoud <hosseiny998@gmail.com>
2021-11-13 21:42:21 +0330

    create dir

--- app/controllers/optimizerController.php
+++ app/controllers/optimizerController.php
@@ -1,12 +1,25 @@
 <?php
 
+require_once '../services/curlService.php';
+
 $path = $_POST['path'];
 
-var_dump($path);
-
 $files = scandir($path);
+$randFolder = rand(10000 , 99999);
+$folderPath = "../../public/result/" .$randFolder;
 
-foreach($files as $file){
-    echo $file;
-    echo "<br>";
+if (mkdir($folderPath)) {
+
+    $curl = new Curl();
+
+    foreach ($files as $file) {
+        $url = getUrl($path . $file , 50);
+        echo $url;
+//        var_dump($curl->getResponse($url));
+        echo "<br>";
+    }
+}
+
+function getUrl($imageLink , $quality){
+    return "http://api.resmush.it/ws.php?img=". $imageLink . "&" ."qlty=". $quality;
 }
--- index.php
+++ index.php
@@ -4,16 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Image Optimizer</title>
 </head>
 <body>
-    <div>
+    <div class="container">
         <h1>Image Optimizer</h1>
         <p>with this tool you can optimize your picture.</p>
         <div>
             <form action="app/controllers/optimizerController.php" method="POST">
-                <input type="text" name="path" placeholder="path"><br>
-                <button type="submit" name="submit">send</button>
+                <div class="form-group">
+                <label for="inputPath">insert dir PATH : </label>
+                    <input type="text" class="form-control-file" name="path" id="inputPath" placeholder="path"><br>
+                    <button type="submit" class="btn btn-primary" name="submit">send</button>
+                </div>
             </form>
         </div>
     </div>
